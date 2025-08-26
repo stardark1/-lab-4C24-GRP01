f675ab90f1cb6f945dac0c4a832c024bab1eba55
stardark1 <joshua.salazar@tecsup.edu.pe>
2025-08-25 20:36:45 -0500

    Modificaciones cheka

--- src/StudentView.java
+++ src/StudentView.java
@@ -1,5 +1,5 @@
 public class StudentView {
-    
+    //gabo loca 
     public void printStudentDetails(StudentModel student) {
         System.out.println("Student: " + student.getName() 
             + " | ID: " + student.getId() 
