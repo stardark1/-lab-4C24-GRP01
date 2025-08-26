df55f783e490caec0123f511f8fd4f56538723b2
stardark1 <joshua.salazar@tecsup.edu.pe>
2025-08-25 20:18:36 -0500

    Modificado StudentController.java: añadido método studentDetails

--- src/controllers/StudentController.java
+++ src/controllers/StudentController.java
@@ -5,4 +5,9 @@ public class StudentController {
     public void printStudent() {
         System.out.println("Student Controller Active");
     }
+
+    public void studentDetails(String name, int age) {
+        System.out.println("Nombre: " + name);
+        System.out.println("Edad: " + age);
+    }
 }
